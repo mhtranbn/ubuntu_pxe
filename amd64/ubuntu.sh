b56b6f1fc6122cc3afb90c1fd018b3d8a4008bf5
mhtran <tranmanhhoang@gmail.com>
2015-10-19 17:33:05 +0700

    Fix ubuntu.sh

--- amd64/ubuntu.sh
+++ amd64/ubuntu.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-#pixiecore -kernel=linux -initrd=initrd.gz
-#qemu -m 1G -kernel ipxe.lkrn
-#title Arch Linux Netboot Environment
-kernel (hd0,0)/ipxe.lkrn
+pixiecore -kernel=linux -initrd=initrd.gz
